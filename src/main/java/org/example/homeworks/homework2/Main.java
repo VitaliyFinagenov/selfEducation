ade7008fcfe6600cf92b2d67c4beaa3dbc2fea76
Vitaliy Finagenov <finagenov1994@gmail.com>
2025-03-21 19:38:51 +0300

    add method hello

--- src/main/java/org/example/homeworks/homework2/Main.java
+++ src/main/java/org/example/homeworks/homework2/Main.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static void replacementNumberInArray() {
-        int[] numbers = {1,1,0,0,1,0,1,1,0,0};
+        int[] numbers = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 1) {
                 numbers[i] = 0;
@@ -34,7 +34,7 @@ public class Main {
     }
 
     public static void multiplicationNumberInArray() {
-        int[] numbers = {1,5,3,2,11,4,5,2,4,8,9,1};
+        int[] numbers = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < 6) {
                 numbers[i] *= 2;
@@ -44,13 +44,17 @@ public class Main {
     }
 
     public static void drowDiagonal() {
-        int[][] twoDimensionalArray = {{5},{5}};
+        int[][] twoDimensionalArray = {{5}, {5}};
         for (int i = 0; i < twoDimensionalArray.length; i++) {
             for (int j = 0; j < twoDimensionalArray.length; j++) {
                 twoDimensionalArray[i][j] = 1;
-                twoDimensionalArray[i][twoDimensionalArray.length -i] = 1;
+                twoDimensionalArray[i][twoDimensionalArray.length - i] = 1;
             }
         }
         System.out.println(Arrays.toString(twoDimensionalArray));
+    }
+
+    public static void hello() {
+        System.out.println("Hello world");
     }
 }

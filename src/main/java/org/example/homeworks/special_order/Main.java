5c9dcef4dec816ff3e70ff06c4209caaa31995cd
SeregaMarkush <s.markushov@icloud.com>
2025-04-18 20:49:07 +0300

    special order

--- src/main/java/org/example/homeworks/special_order/Main.java
+++ src/main/java/org/example/homeworks/special_order/Main.java
@@ -1,11 +1,25 @@
 package org.example.homeworks.special_order;
 
 
+import java.util.List;
+
 /**
  * Воспроизведи создание бутерброда с сосиской, майонезом и луком.
  */
 public class Main {
     public static void main(String[] args) {
-        Base bread = new Base(" ");
+        Base bread = new Base("хлебушек");
+        MainIngredient sausage = new MainIngredient("сосиска");
+        Souse mayones = new Souse("майнез");
+        SideIngredient onion = new SideIngredient("лук");
+        Dish sandwich = new Dish(
+                "бутерброд",
+                bread,
+                sausage,
+                List.of(mayones),
+                List.of(onion)
+        );
+
+        System.out.println(sandwich.toString());
     }
 }
